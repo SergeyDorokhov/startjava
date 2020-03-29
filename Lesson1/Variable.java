230800fb8dc87a877a6b5e1e5f4e4cbaee056a07
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-03-29 18:56:11 +0300

    Create Variable class

---
+++ Lesson1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte table = 1;
+		short  disk = 2;
+		int windows = 10;
+		long core = 2;
+		float cpuSpeed = 3.00f;
+		double memory = 8.00d;
+		char computerName = 'S';
+		boolean isNotebook = true;
+		System.out.println(table);
+		System.out.println(disk);
+		System.out.println(windows);
+		System.out.println(core);
+		System.out.println(cpuSpeed);
+		System.out.println(memory);
+		System.out.println(computerName);
+		System.out.println(isNotebook);
+	}
+}
