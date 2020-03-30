b155ddec123a3db22f3f92807c2ce40df934d4d3
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-03-30 14:19:01 +0300

    fixed bugs

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,13 +1,14 @@
 public class Variable {
 	public static void main(String[] args) {
 		byte table = 1;
-		short  disk = 2;
+		short disk = 2;
 		int windows = 10;
 		long core = 2;
 		float cpuSpeed = 3.00f;
 		double memory = 8.00d;
 		char computerName = 'S';
 		boolean isNotebook = true;
+		
 		System.out.println(table);
 		System.out.println(disk);
 		System.out.println(windows);
