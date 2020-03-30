929205f2c3f8cfef1cc7aaeb41c4263687399b4c
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-03-30 17:50:40 +0300

    create Cycle class

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,23 @@
+public class Cycle {
+	public static void main(String[] args) {
+		for (int i = 0; i < 21; i++) {
+			System.out.println(i);
+		}
+
+		int i = 6;
+		while (i >= -6) {
+			System.out.println(i);
+			i = i - 2;
+		}
+
+		i = 10;
+		int sum = 0;
+		do {
+			if (i % 2 != 0) {
+				sum = sum + i;
+			}
+			i++;
+		} while (i <= 20);
+		System.out.println(sum);
+	}
+}
