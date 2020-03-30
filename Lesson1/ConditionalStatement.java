8706d4b36b13d9ca41fe653ae008f3c8ddcf048f
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-03-30 17:49:04 +0300

    bugfix ConditionalStatement

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,27 +1,29 @@
 public class ConditionalStatement {
 	public static void main(String[] args) {
 		int age = 25;
-		boolean isMan = true;
-		float height = 1.75f;
-		char firstLetter = 'M';
-
 		if (age > 20) {
 			System.out.println("Уже старый");
 		}
-		if (isMan == true) {
+
+		boolean isMan = true;
+		if (isMan) {
 			System.out.println("Это мужчина");
 		}
-		if (isMan != true) {
+		if (!isMan) {
 			System.out.println("Это не мужчина");
 		}
+
+		float height = 1.75f;
 		if (height < 1.80) {
 			System.out.println("Подойдет в танкисты");
 		} else {
 			System.out.println("Не подойдет в танкисты");
 		}
-		if (firstLetter == 'M') {
+
+		char firstNameLetter = 'M';
+		if (firstNameLetter == 'M') {
 			System.out.println("Это Марфа");
-		} else if (firstLetter == 'I') {
+		} else if (firstNameLetter == 'I') {
 			System.out.println("Это не Марфа");
 		} else {
 			System.out.println("Не знаю, кто это");
