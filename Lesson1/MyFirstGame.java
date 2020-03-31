797434a3f4e10efd1949450b52eb60f668e99ab4
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-03-31 13:32:33 +0300

    bugfix MyFirstGame

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -2,21 +2,18 @@ public class MyFirstGame {
 	public static void main(String[] args) {
 		int hiddenNumber = 43;
 		int declaredNumber = 50;
-		if (declaredNumber == hiddenNumber) {
-			System.out.println ("Вы ввели: " + declaredNumber + ". Вы угадали!");
-		} else {
-			do {
-				if (declaredNumber > hiddenNumber) {
+
+		do {
+			if (declaredNumber > hiddenNumber) {
 				System.out.println ("Вы ввели: " + declaredNumber + ". Загадано меньшее число!");
 				declaredNumber--;
-				} else if (declaredNumber < hiddenNumber) {
+			} else if (declaredNumber < hiddenNumber) {
 				System.out.println ("Вы ввели: " + declaredNumber + ". Загадано большее число!");
 				declaredNumber++;
-				}
-				if (declaredNumber == hiddenNumber) {
-					System.out.println ("Вы ввели: " + declaredNumber + ". Вы угадали!");
-				}
-			} while (declaredNumber != hiddenNumber);
-		}
+			}
+			if (declaredNumber == hiddenNumber) {
+				System.out.println ("Вы ввели: " + declaredNumber + ". Вы угадали!");
+			}
+		} while (declaredNumber != hiddenNumber);
 	}
 }
