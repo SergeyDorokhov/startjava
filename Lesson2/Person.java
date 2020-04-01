43d7b368897bfda880f29e48d798fef44073eee8
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-04-01 18:17:00 +0300

    create Person class

---
+++ Lesson2/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+	String name = "Alex";
+	int age = 25;
+	char sex = 'M';
+	float height = 1.80f;
+	float weight = 80.5f;
+
+	
+	public void walk() {
+
+	}
+	public void sit() {
+
+	}
+	public void run() {
+
+	}
+	public void speak() {
+
+	}
+	public void learnJava() {
+
+	}
+}
