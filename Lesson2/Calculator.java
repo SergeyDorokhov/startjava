38b15fdf7711ecbb5a0bd4b5a87ae7709b091c31
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-04-04 17:45:50 +0300

    refactoring Calculator and test

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class Calculator {
 	private int firstNumber;
-	private int secondNamber;
+	private int secondNumber;
 	private char operation;
 
 	public int getFirstNumber() {
@@ -12,11 +14,11 @@ public class Calculator {
 	}
 
 	public int getSecondNumber() {
-		return secondNamber;
+		return secondNumber;
 	}
 
-	public void setSecondNumber(int secondNamber) {
-		this.secondNamber = secondNamber;
+	public void setSecondNumber(int secondNumber) {
+		this.secondNumber = secondNumber;
 	}
 
 	public char getOperation() {
@@ -25,5 +27,45 @@ public class Calculator {
 
 	public void setOperation(char operation) {
 		this.operation = operation;
+	}
+
+	public char calculate() {
+		switch (operation) {
+			case '+':
+				System.out.println(this.firstNumber + this.secondNumber);
+				break;
+			case '-':
+				System.out.println(firstNumber - secondNumber);
+				break;
+			case '*':
+				System.out.println(firstNumber * secondNumber);
+				break;
+			case '/':
+				System.out.println(firstNumber / secondNumber);
+				break;
+			case '%':
+				System.out.println(firstNumber % secondNumber);
+				break;
+			case '^':
+				int result = 1;
+				for (int i = 1; i <= secondNumber; i++ ) {
+					result *= firstNumber;
+				}
+				System.out.println(result);
+				break;
+			default: System.out.println("Неизвестный знак");
+				break;
+		}
+		Scanner scan;
+		char userReply;
+		while (true) {
+			System.out.println("Хотите продолжить? [y/n]: ");
+			scan = new Scanner(System.in);
+			userReply = scan.nextLine().charAt(0);
+			if (userReply == 'y' || userReply == 'n') {
+				break;
+			}
+		}
+	return userReply;
 	}
 }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -2,12 +2,11 @@ import java.util.*;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
-		boolean isWorking = true;
-		while(isWorking) {
-			Calculator calc = new Calculator();
-		
+		Calculator calc = new Calculator();
+		Scanner scan = new Scanner(System.in);
+		char userReply;
+		do {
 			System.out.print("Введите первое число: ");
-			Scanner scan = new Scanner(System.in);
 			int firstNumber = scan.nextInt();
 			calc.setFirstNumber(firstNumber);
 
@@ -17,47 +16,10 @@ public class CalculatorTest {
 			calc.setOperation(operation);
 
 			System.out.print("Введите второе число: ");
-			scan = new Scanner(System.in);
 			int secondNumber = scan.nextInt();
 			calc.setSecondNumber(secondNumber);
 
-			switch (operation) {
-				case '+':
-					System.out.println(calc.getFirstNumber() + calc.getSecondNumber());
-					break;
-				case '-':
-					System.out.println(calc.getFirstNumber() - calc.getSecondNumber());
-					break;
-				case '*':
-					System.out.println(calc.getFirstNumber() * calc.getSecondNumber());
-					break;
-				case '/':
-					System.out.println(calc.getFirstNumber() / calc.getSecondNumber());
-					break;
-				case '%':
-					System.out.println(calc.getFirstNumber() % calc.getSecondNumber());
-					break;
-				case '^':
-					int result = 1;
-					for (int i = 1; i <= calc.getSecondNumber(); i++ ) {
-						result *= calc.getFirstNumber();
-					}
-					System.out.println(result);
-					break;
-				default: System.out.println("Неизвестный знак");
-					break;
-			}
-			while (true) {
-				System.out.println("Хотите продолжить? [y/n]: ");
-				scan = new Scanner(System.in);
-				char userReply = scan.nextLine().charAt(0);
-				if (userReply == 'y') {
-					break;
-				} else if (userReply == 'n') {
-					isWorking = false;
-					break;
-				}
-			}
-		}
+			userReply = calc.calculate();
+		} while (userReply != 'n');
 	}
 }
