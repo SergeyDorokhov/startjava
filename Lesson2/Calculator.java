fd46ec0b8c6ddbc887ca5171149848e43fd1f11f
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-04-04 16:06:09 +0300

    fix getters and setters

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -7,20 +7,20 @@ public class Calculator {
 		return firstNumber;
 	}
 
-	public int getSecondNumber() {
-		return secondNamber;
-	}
-
-	public char getOperation() {
-		return operation;
-	}
-
 	public void setFirstNumber(int firstNumber) {
 		this.firstNumber = firstNumber;
 	}
 
+	public int getSecondNumber() {
+		return secondNamber;
+	}
+
 	public void setSecondNumber(int secondNamber) {
 		this.secondNamber = secondNamber;
+	}
+
+	public char getOperation() {
+		return operation;
 	}
 
 	public void setOperation(char operation) {
