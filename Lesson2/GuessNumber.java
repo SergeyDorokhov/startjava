1e2a6cfa4aa400928644b386a4c3f6ee098c6450
SergeyDorokhov <dorokhov-sergey@yandex.ru>
2020-04-05 21:22:27 +0300

    bugfix GuessNumber

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class GuessNumber {
 	private int hiddenNumber;
 
@@ -5,8 +7,33 @@ public class GuessNumber {
 		hiddenNumber = (int)(Math.random() * 101);
 	}
 
-	public boolean checkNumber (int declaredNumber) {
+	public void startGame(Player playerOne, Player playerTwo) {
+		Scanner scan = new Scanner(System.in);
+		char userReply;
+
+		System.out.println("Компьютер загадал число от нуля до 100! Ваша задача - угадать это число!");
+		do {
+			while (true) {
+				if (isGuessed(playerOne)) {
+					break;
+				}
+				if (isGuessed(playerTwo)) {
+					break;
+				}
+			}
+			do {
+			System.out.println("Еще одну игру? [y/n]: ");
+			userReply = scan.nextLine().charAt(0);
+			} while (userReply != 'y' && userReply != 'n');
+		} while (userReply != 'n');
+	}
+
+	private boolean isGuessed (Player player) {
+		Scanner scan = new Scanner(System.in);
 		boolean isVictory = false;
+		
+		System.out.println(player.getName() + ", введите число!");
+		int declaredNumber = scan.nextInt();
 		if (declaredNumber > hiddenNumber) {
 				System.out.println ("Вы ввели: " + declaredNumber + ". Загадано меньшее число!");
 			} else if (declaredNumber < hiddenNumber) {
--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -4,33 +4,10 @@ public class GuessNumberTest {
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		char userReply;
-		
 		System.out.println("Друзья, начинаем игру! Введите имя первого игрока:");
 		Player playerOne = new Player(scan.nextLine());
 		System.out.println("Введите имя второго игрока:");
 		Player playerTwo = new Player(scan.nextLine());
-
-		System.out.println("Просим компьютер загадать число от нуля до 100!");
-		GuessNumber guessNumber = new GuessNumber();
-		System.out.println("Компьютер загадал число! Начинает " + playerOne.getName());
-
-		do {
-			while (true) {
-				System.out.println(playerOne.getName() + ", введите число!");
-				if (guessNumber.checkNumber(scan.nextInt())) {
-					break;
-				}
-				System.out.println(playerTwo.getName() + ", введите число!");
-				if (guessNumber.checkNumber(scan.nextInt())) {
-					break;
-				}
-			}
-			scan.nextLine();
-			do {
-				System.out.println("Еще одну игру? [y/n]: ");
-				userReply = scan.nextLine().charAt(0);
-			} while (userReply != 'y' && userReply != 'n');
-		} while (userReply != 'n');
+		(new GuessNumber()).startGame(playerOne, playerTwo);
 	}
 }
